94bd3423cfe763440fd53ea90cd1152045d61747
Alistair Kung <alistaircckung@gmail.com>
2017-07-05 17:10:15 +0100

    added colours depending on energy usage

--- src/interface.js
+++ src/interface.js
@@ -1,21 +1,25 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
   $("#power-saving-status").text(thermostat.getEnergyMode());
-  $('#temperature').text(thermostat.getTemp());
+  updateTemperature()
   $("#temperature-up").click(function() {
     thermostat.up(1);
-    $("#temperature").text(thermostat.getTemp());
+    updateTemperature()
   });
   $("#temperature-down").click(function() {
     thermostat.down(1);
-    $("#temperature").text(thermostat.getTemp());
+    updateTemperature()
   });
   $("#temperature-reset").click(function() {
     thermostat.resetTemp();
-    $("#temperature").text(thermostat.getTemp());
+    updateTemperature()
   });
   $("#powersaving-toggle").click(function() {
     thermostat.togglePowerSaving();
     $("#power-saving-status").text(thermostat.getEnergyMode());
   });
+  function updateTemperature() {
+    $('#temperature').text(thermostat.getTemp());
+    $('#temperature').attr('class', thermostat.energyUsage());
+  }
 });
